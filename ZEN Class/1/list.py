64f50b2996bab1a7917f04bbc2b7e214245cfb96
yuva671 <57931840+yuva671@users.noreply.github.com>
2019-11-19 14:03:15 +0530

    Add files via upload

---
+++ ZEN Class/1/list.py
@@ -0,0 +1,6 @@
+for i in range(0,5):
+    m=[]
+    n=int(input())
+    m.append(n)
+    print(m)
+    
---
+++ ZEN Class/1/listAvg.py
@@ -0,0 +1,11 @@
+s=0
+m=list()
+for i in range(0,5):
+    
+    n=int(input())
+    m.append(n)
+    s=s+m[i]
+   
+print(s)  
+avg=s/5
+print(avg)
